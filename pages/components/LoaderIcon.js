389b8199a2485f5554102f3f4307a5423a47ca93
netstermonir <monirhossain6163@gmail.com>
2023-02-10 16:40:03 +0600

    first commit

---
+++ pages/components/LoaderIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoaderIcon = () => {
+    return (
+        <div className="la-ball-beat text-xs top-5 lg:top-6 right-5">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default LoaderIcon;
